0fe3a4e152b92a7a389bc09e3c387c42357e46e6
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2024-02-28 20:57:59 +0530

    Remove redundant!

--- device.mk
+++ device.mk
@@ -77,12 +77,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
-    AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
-    POSTINSTALL_OPTIONAL_vendor=true
-
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl \
