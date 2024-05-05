27306878f812b54c81094d973da6c4ec8227f7b1
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2024-05-05 18:28:28 +0530

    Fix build error!  * error: missing separator.

--- device.mk
+++ device.mk
@@ -68,7 +68,8 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
 
-# Update engine    
+# Update engine 
+PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
